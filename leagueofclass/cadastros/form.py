afcb55f8937a693b94a976846f29168e73761155
osvaldoAiron <osvaldo_cavalcanti_filho@hotmail.com>
2018-09-15 16:54:42 -0300

    formProfessor

--- leagueofclass/cadastros/form.py
+++ leagueofclass/cadastros/form.py
@@ -1,20 +1,22 @@
 from django import forms
 from .models import Professor
 from .models import Aluno
+from django.forms import ModelForm
 
 
 
-class ProfessorForm(forms.ModelForm):
+class ProfessorForm(ModelForm):
 	'''
 	Responsavel por criar o modelo de formulario a partir do modelo que esta sendo criado no db;
 	'''
+
 	class Meta:
 		model = Professor
 		fields = '__all__'
 
 
-'''
-class AlunoForm(forms.ModelForm):
+
+class AlunoForm(ModelForm):
 	class Meta:
 		model = Aluno
-'''
+		fields = '__all__'
