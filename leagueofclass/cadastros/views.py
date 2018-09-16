951d80a0fffb82b0cda0c533ca399997835e2568
osvaldoAiron <osvaldo_cavalcanti_filho@hotmail.com>
2018-09-15 22:11:14 -0300

    criando_users

--- leagueofclass/cadastros/views.py
+++ leagueofclass/cadastros/views.py
@@ -5,20 +5,45 @@ from django.views.generic.edit import CreateView
 from django.shortcuts import redirect
 from django.contrib import messages
 from time import sleep
+from django.contrib.auth.models import User
+from django.shortcuts import HttpResponseRedirect
+from django.views.decorators.http import require_POST
 # Create your views here.
+
 
 
 def cadastroProfessor(request):
 	#url cadastro/ retorna o template de cadastros Professor!
 	#Ñ implementada ainda;
+
 	
 	if request.method=="POST":
 		form = ProfessorForm(request.POST)
+
 		if form.is_valid():
-			form.save()
-			messages.success(request, 'Dados cadastrados com sucesso!')
-			sleep(1)
-			return redirect('/home')
+
+				try:
+
+					verificaExistenciaPass = User.objects.get(password=form.cleaned_data['password'])
+					verificaExistencia = User.objects.get(email=form.cleaned_data['email'])
+					if verificaExistenciaPass or verificaExistencia:
+						return render(request,'/home', {'msg': 'Ja existe um usuario com o mesmo email!'})
+
+
+				except User.DoesNotExist:
+					nome_professor = form.cleaned_data['nome']
+					email_professor = form.cleaned_data['email']
+					password_professor = form.cleaned_data['password']
+					new_professor = User.objects.create_user(username=nome_professor, email=email_professor,
+															 password=password_professor)
+
+					new_professor.save()
+					form.save()
+					return redirect("/home")
+
+
+
+
 		else:
 			messages.warning(request, 'Preencha todos os campos corretamente!')
 			
@@ -41,9 +66,26 @@ def cadastroAluno(request):
 		'''
 
 		if form.is_valid():
-			form.save();
-			sleep(1)
-			return redirect('/home')
+			try:
+
+				verificaExistenciaPass = User.objects.get(password=form.cleaned_data['password'])
+				verificaExistencia = User.objects.get(email=form.cleaned_data['email'])
+				if verificaExistenciaPass or verificaExistencia:
+					return render(request, '/home', {'msg': 'Ja existe um usuario com o mesmo email!'})
+
+
+			except User.DoesNotExist:
+				nome_aluno = form.cleaned_data['nome']
+				email_aluno = form.cleaned_data['email']
+				password_aluno = form.cleaned_data['password']
+				new_aluno = User.objects.create_user(username=nome_aluno, email=email_aluno,
+														 password=password_aluno)
+
+				new_aluno.save()
+				form.save()
+				return redirect("/home")
+		else:
+			messages.warning(request, 'Preencha todos os campos corretamente!')
 	else:
 		form = AlunoForm()
 
